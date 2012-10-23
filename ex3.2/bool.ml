8a2e8e0bebdb7acacf519c066052509e500bd31f
Kazuya Numata <numata@gmail.com>
2012-10-23 23:47:43 +0900

    Answer ex3.2

---
+++ ex3.2/bool.ml
@@ -0,0 +1,13 @@
+let orig_and b1 b2 =
+  b1 && b2;;
+
+let answer_and b1 b2 =
+  if b1 then b2 else false;;
+
+let orig_or b1 b2 =
+  b1 || b2;;
+
+let answer_or b1 b2 =
+  if b1 then true else b2;;
+
+
