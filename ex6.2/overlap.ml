39f3915a48f35aae9da380869a512c403e9fd100
Kazuya Numata <numata@gmail.com>
2012-10-30 22:15:19 +0900

    Refactoring

--- ex6.2/overlap.ml
+++ ex6.2/overlap.ml
@@ -15,19 +15,24 @@ let overlap_point_circle f1 f2 =
 
 let overlap_point_rectangle f1 f2 =
     let {loc_x = loc_x; loc_y = loc_y; body = Rectangle (x, y)} = f2 in
-    (loc_x -. float_of_int(x) /. 2.0 <= f1.loc_x) && (loc_x +. float_of_int(x) /. 2.0 >= f1.loc_x) && (loc_y -. float_of_int(y) /. 2.0 <= f1.loc_y) && (loc_y +. float_of_int(y) /. 2.0 >= f1.loc_y);;
+    (loc_x -. float_of_int(x) /. 2.0 <= f1.loc_x) &&
+      (loc_x +. float_of_int(x) /. 2.0 >= f1.loc_x) &&
+      (loc_y -. float_of_int(y) /. 2.0 <= f1.loc_y) &&
+      (loc_y +. float_of_int(y) /. 2.0 >= f1.loc_y);;
+
 let overlap_circle_circle f1 f2 =
   match (f1.body, f2.body) with
       (Circle r1, Circle r2) ->
 	(f1.loc_x -. f2.loc_x) ** 2.0 +. (f1.loc_y -. f2.loc_y) ** 2.0 <= float_of_int (2 * (r1 + r2))
     | (_, _) -> false;;
 
-let four_points f =
+let corners_of_rectangle f =
   let {loc_x = rx; loc_y = ry; body = Rectangle (x, y)} = f in
   ((rx -. (float_of_int x) /. 2.0, ry -. (float_of_int y) /. 2.0),
    (rx -. (float_of_int x) /. 2.0, ry +. (float_of_int y) /. 2.0),
    (rx +. (float_of_int x) /. 2.0, ry -. (float_of_int y) /. 2.0),
    (rx +. (float_of_int x) /. 2.0, ry +. (float_of_int y) /. 2.0));;
+
 let in_circle point cir =
   let px = fst point in
   let py = snd point in
@@ -43,8 +48,7 @@ let in_rectangle point rect =
 
 let overlap_circle_rectangle f1 f2 =
   let {loc_x = cx; loc_y = cy; body = Circle r} = f1 in
-  let {loc_x = rx; loc_y = ry; body = Rectangle (x, y)} = f2 in
-  let (c1, c2, c3, c4) = four_points f2 in
+  let (c1, c2, c3, c4) = corners_of_rectangle f2 in
   in_circle c1 f1 || in_circle c2 f1 || in_circle c3 f1 || in_circle c4 f1 ||
     in_rectangle (cx -. float_of_int r, cy) f2 ||
     in_rectangle (cx +. float_of_int r, cy) f2 ||
@@ -56,16 +60,16 @@ let overlap_rectangle_rectangle f1 f2 =
   let {loc_x = rx1; loc_y = ry1; body = Rectangle (x1, y1)} = f1 in
   let {loc_x = rx2; loc_y = ry2; body = Rectangle (x2, y2)} = f2 in
   if x1 * y1 < x2 * y2 then
-    let (c1, c2, c3, c4) = four_points f1 in
+    let (c1, c2, c3, c4) = corners_of_rectangle f1 in
     in_rectangle c1 f2 || in_rectangle c2 f2 || in_rectangle c3 f2 || in_rectangle c4 f2
   else
-    let (c1, c2, c3, c4) = four_points f2 in
+    let (c1, c2, c3, c4) = corners_of_rectangle f2 in
     in_rectangle c1 f1 || in_rectangle c2 f1 || in_rectangle c3 f1 || in_rectangle c4 f1;;
 
 let rectangle_of_square f =
   match f.body with
       Square x' ->
-	{loc_x = f.loc_x; loc_y = f.loc_y; body = Rectangle(x', x')}
+	{f with body = Rectangle (x',x')}
     | _ ->
       f;;
 
