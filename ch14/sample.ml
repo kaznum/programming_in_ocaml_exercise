7c8698ea596b2f887a2e28080159be5785652331
Kazuya Numata <numata@gmail.com>
2012-11-24 23:04:59 +0900

    Implement ch14's sample (in progress)

--- ch14/sample.ml
+++ ch14/sample.ml
@@ -55,3 +55,61 @@ add_A [`B];;
 
 (* error *)
 add_A [`A 1];;
+
+let singleton_A x =
+  let _ = `A :: x in
+  [`A];;
+
+singleton_A [`B];;
+(singleton_A : [`A | `B] list -> [`A | `C] list);;
+
+(* error *)
+(add_A : [`A | `B] list -> [`A | `C] list);;
+
+
+let c = ref [`Can];;
+c := (`May : [`Can | `May]) :: !c;;
+c;;
+(* error *)
+c := `Will :: !c;;
+
+
+let c = ref [];;
+c := 1 :: !c;;
+c;;
+(* error *)
+c := true :: !c;;
+
+
+type seasons = [ `Spring | `Summer | `Autumn | `Winter];;
+(* error *)
+type seasons = [> `Spring | `Summer | `Autumn | `Winter];;
+
+type seasons = [ `Spring | `Summer | `Autumn | `Winter];;
+type seasons_of_japan = [ seasons | `Tsuyu];;
+
+let hito' = function
+    `Forward -> "walking forward"
+  | `Backward -> "walking backward"
+  | (`Right | `Left) as x -> kani x;;
+
+hito' `Right;;
+
+let hito' = function
+    `Forward -> "walking forward"
+  | `Backward -> "walking backward"
+  | x -> kani x;;
+
+type kani_dir = [`Left | `Right];;
+let hito' = function
+    `Forward -> "walking forward"
+  | `Backward -> "walking backward"
+  | #kani_dir as x -> kani x;;
+
+
+type sample_var = [`Hoge of int * string | `Fuga of string * int];;
+let hito'' = function
+    `Forward -> "walking forward"
+  | `Backward -> "walking backward"
+  | #kani_dir as x -> kani x
+  | #sample_var -> "sample_var";;
