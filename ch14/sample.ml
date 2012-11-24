7ef4c61d653ed904684fc405225d99c6e17cd9b8
Kazuya Numata <numata@gmail.com>
2012-11-24 16:55:59 +0900

    Implement ch14's sample (in progress)

--- ch14/sample.ml
+++ ch14/sample.ml
@@ -7,3 +7,51 @@ fun x -> if x then `Can else `May;;
 [`Can; `May];;
 
 [`Can 2; `Bottle; `Can 3];;
+
+let c = `Can and m = `May and b = `Bottle;;
+
+[c; m];;
+
+[c; b];;
+
+(* error *)
+fun x -> if x then `Can 2 else `Can true;;
+[`Can 2; `Can true];;
+
+(* success *)
+(`Can 2, `Can true);;
+
+let kani = function
+    `Right -> "walking to the right"
+  | `Left -> "walking to the left";;
+
+let hito = function
+    `Right -> "walking to the right"
+  | `Left -> "walking to the left"
+  | `Forward -> "walking forward"
+  | `Backward -> "walking backward";;
+
+(* error *)
+let c : [`Can] = `Can and m = `May in
+[m; c];;
+let nil : int list = [] in
+true :: nil;;
+
+[kani; hito];;
+
+
+let mirror = function
+    `Right -> `Left
+  | `Left -> `Right
+  | x -> x;;
+
+let mirror' = function
+    `Right -> `Left
+  | `Left -> `Right;;
+
+let add_A x = `A :: x;;
+
+add_A [`B];;
+
+(* error *)
+add_A [`A 1];;
